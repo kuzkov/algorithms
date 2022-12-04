b6a707c4ccfc37c337bd0ee16b3793eae4c479cb
Artyom Kuzkov <artyom.kuzkov@yandex.ru>
2022-12-05 00:33:46 +0200

    Add group by query

--- codewars/sql-engine.js
+++ codewars/sql-engine.js
@@ -3,6 +3,7 @@ const query = () => {
   const defaultData = [];
 
   let selector = null;
+  let groupingSelector = null;
   let data = null;
   let filters = [];
 
@@ -25,7 +26,7 @@ const query = () => {
 
   const from = (currentData) => {
     if (currentData === undefined && currentData.length === undefined) {
-      throw new Error("Invalid type of 'from' argument");
+      throw new Error("Invalid type of FROM argument");
     }
 
     if (data) {
@@ -47,26 +48,54 @@ const query = () => {
     return queryEngine;
   };
 
+  const groupBy = (selector) => {
+    if (!selector || typeof selector !== "function") {
+      throw new Error("Invalid grouping selector argument");
+    }
+
+    groupingSelector = selector;
+
+    return queryEngine;
+  };
+
   const execute = () => {
     const finalSelector = selector ?? defaultSelector;
-    const finalData = data ?? defaultData;
 
-    const filteredData = filters.reduce(
+    let result = data ?? defaultData;
+
+    result = filters.reduce(
       (data, filter) => data.filter((item) => filter(item)),
-      finalData
+      result
     );
 
-    const selectedData = filteredData.map((item) => finalSelector(item));
+    if (groupingSelector) {
+      const grouped = [];
 
-    return selectedData;
+      for (let item of result) {
+        const groupingValue = groupingSelector(item);
+        const group = grouped.find(([key]) => key === groupingValue);
+
+        if (group) {
+          group[1].push(item);
+        } else {
+          grouped.push([groupingValue, [item]]);
+        }
+      }
+
+      result = grouped;
+    }
+
+    result = result.map((item) => finalSelector(item));
+
+    return result;
   };
 
   const queryEngine = {
     select,
     from,
     where,
+    groupBy,
     orderBy: () => {},
-    groupBy: () => {},
     having: () => {},
     execute,
   };
@@ -152,9 +181,165 @@ function isTeacher(person) {
   return person.profession === "teacher";
 }
 
+// SELECT profession FROM persons WHERE profession="teacher"
 const result6 = query()
   .select(profession)
   .from(persons)
   .where(isTeacher)
   .execute();
 console.log(_.isEqual(result6, ["teacher", "teacher", "teacher"]));
+
+// SELECT * FROM persons WHERE profession="teacher"
+const result7 = query().select().from(persons).where(isTeacher).execute();
+const test7 = persons.filter((person) => person.profession === "teacher");
+console.log(_.isEqual(result7, test7));
+
+// SELECT name FROM persons WHERE profession="teacher"
+const nameSelector = (person) => person.name;
+const result8 = query()
+  .select(nameSelector)
+  .from(persons)
+  .where(isTeacher)
+  .execute();
+console.log(_.isEqual(result8, ["Peter", "Michael", "Peter"]));
+
+// Group by
+const result9 = query().select().from(persons).groupBy(profession).execute();
+const test9 = [
+  [
+    "teacher",
+    [
+      {
+        name: "Peter",
+        profession: "teacher",
+        age: 20,
+        maritalStatus: "married",
+      },
+      {
+        name: "Michael",
+        profession: "teacher",
+        age: 50,
+        maritalStatus: "single",
+      },
+      {
+        name: "Peter",
+        profession: "teacher",
+        age: 20,
+        maritalStatus: "married",
+      },
+    ],
+  ],
+  [
+    "scientific",
+    [
+      {
+        name: "Anna",
+        profession: "scientific",
+        age: 20,
+        maritalStatus: "married",
+      },
+      {
+        name: "Rose",
+        profession: "scientific",
+        age: 50,
+        maritalStatus: "married",
+      },
+      {
+        name: "Anna",
+        profession: "scientific",
+        age: 20,
+        maritalStatus: "single",
+      },
+    ],
+  ],
+  [
+    "politician",
+    [
+      {
+        name: "Anna",
+        profession: "politician",
+        age: 50,
+        maritalStatus: "married",
+      },
+    ],
+  ],
+];
+console.log(_.isEqual(result9, test9));
+
+// SELECT * FROM persons WHERE profession='teacher' GROUP BY profession
+const result10 = query()
+  .select()
+  .from(persons)
+  .where(isTeacher)
+  .groupBy(profession)
+  .execute();
+// console.log(result10);
+
+// SELECT with GROUP BY
+const professionGroup = (group) => group[0];
+const result11 = query()
+  .select(professionGroup)
+  .from(persons)
+  .groupBy(profession)
+  .execute();
+console.log(_.isEqual(result11, ["teacher", "scientific", "politician"]));
+
+// Group by example
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+function parity(number) {
+  return isEven(number) ? "even" : "odd";
+}
+
+const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// SELECT * FROM numbers
+console.log(
+  _.isEqual(
+    query().select().from(numbers2).execute(),
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  )
+);
+
+// SELECT * FROM numbers GROUP BY parity
+console.log(
+  _.isEqual(query().select().from(numbers2).groupBy(parity).execute(), [
+    ["odd", [1, 3, 5, 7, 9]],
+    ["even", [2, 4, 6, 8]],
+  ])
+);
+
+// Multilevel grouping
+function isPrime(number) {
+  if (number < 2) {
+    return false;
+  }
+  var divisor = 2;
+  for (; number % divisor !== 0; divisor++);
+  return divisor === number;
+}
+
+function prime(number) {
+  return isPrime(number) ? "prime" : "divisible";
+}
+
+console.log(
+  _.isEqual(query().select().from(numbers).groupBy(parity, prime).execute(), [
+    [
+      "odd",
+      [
+        ["divisible", [1, 9]],
+        ["prime", [3, 5, 7]],
+      ],
+    ],
+    [
+      "even",
+      [
+        ["prime", [2]],
+        ["divisible", [4, 6, 8]],
+      ],
+    ],
+  ])
+);
